5fb291685cc36883e355c244769f87943ce66be9
Leandro Carvalho <dev.leandrocarvalho@gmail.com>
2022-12-02 14:42:37 -0300

    log out

--- front/src/pages/components/Header.tsx
+++ front/src/pages/components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import Logo from '../../../public/images/logo.svg';
 import Out from '../../../public/images/out.svg';
 import Profile from '../../../public/images/profile.svg';
@@ -11,7 +12,9 @@ export default function Header({ setShowModal }: any) {
 				<Image priority src={Logo} alt='logo' width={120} height={120} />
 				<div>
 					<Image priority src={Profile} style={{ cursor: 'pointer' }} alt='profile' width={50} height={50} onClick={() => setShowModal(true)} />
-					<Image priority src={Out} alt='out' width={27} height={27} style={{ cursor: 'pointer' }} />
+					<Link href='/'>
+						<Image priority src={Out} alt='out' width={27} height={27} style={{ cursor: 'pointer' }} />
+					</Link>
 				</div>
 			</div>
 		</main>
