b789bd3831b4002300ab14e198740e8562ae3321
Leandro Carvalho <dev.leandrocarvalho@gmail.com>
2022-12-15 09:56:22 -0300

    changes

--- back/dist/repositories/categoryRepository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.categoryRepository = void 0;
-var data_source_1 = require("../data-source");
-var Category_1 = require("../entities/Category");
-exports.categoryRepository = data_source_1.AppDataSource.getRepository(Category_1.Category);
