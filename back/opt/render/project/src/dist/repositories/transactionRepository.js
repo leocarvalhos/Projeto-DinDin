7cf8fc0f48c172b2c1a525defb850733f25e7ac3
Leandro Carvalho <dev.leandrocarvalho@gmail.com>
2022-12-15 08:30:11 -0300

    c

--- back/opt/render/project/src/dist/repositories/transactionRepository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.transactionRepository = void 0;
-const data_source_1 = require("../data-source");
-const Transaction_1 = require("../entities/Transaction");
-exports.transactionRepository = data_source_1.AppDataSource.getRepository(Transaction_1.Transaction);
