62a8b4cd240cdf352c0e1434dad1fdc814ad09ab
Leandro <dev.leandrocarvalho@gmail.com>
2022-12-23 15:47:24 -0300

    changes

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://dindin.onrender.com',
     timeout: 120000,
     headers: { 'Content-Type': 'application/json' },
 });
